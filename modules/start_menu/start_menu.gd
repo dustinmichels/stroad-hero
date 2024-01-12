618950ef54326b8fea77c14a3fa905242343ab17
Dustin Michels <dustin7538@gmail.com>
2024-01-12 00:42:05 -0500

    add start menu

---
+++ modules/start_menu/start_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+@onready var start_button = %StartButton as Button
+@onready var quit_button = %QuitButton as Button
+@onready var main_game = preload("res://main.tscn") as PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_button.button_down.connect(on_start_button_pressed)
+	quit_button.button_down.connect(on_quit_button_pressed)
+	
+
+func on_start_button_pressed():
+	get_tree().change_scene_to_packed(main_game)
+	
+func on_quit_button_pressed():
+	get_tree().quit()
+
